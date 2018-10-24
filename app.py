5fe7a3a35237ab24023482504560b703b95b0225
prashanthrebala <prashanth.rebala@gmail.com>
2018-10-24 15:27:12 +0530

    ready for dep

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from code_to_name import name_from_url
+
+app = Flask(__name__)
+
+
+@app.route('/invite/<code>')
+def hello_world(code):
+    return render_template("invite.html", name=name_from_url[code])
+
+
+if __name__ == '__main__':
+    app.run()
