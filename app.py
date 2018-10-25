58d4696742aaa5184cacc06c5bb32d01562af728
prashanthrebala <prashanth.rebala@gmail.com>
2018-10-25 21:48:08 +0530

    complete

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from code_to_name import name_from_url
 app = Flask(__name__)
 
 
+@app.route('/3120234xd', methods=['GET'])
+def better_luck_next_time():
+    return render_template("better_luck.html")
+
+
 @app.route('/<code>', methods=['GET'])
 def hello_world(code):
-    return render_template("invite.html", name=name_from_url[code])
+    try:
+        return render_template("invite.html", name=name_from_url[code])
+    except KeyError:
+        return "<h1> Invalid invite ID: <code>{0}</code></h1>".format(code)
 
 
 if __name__ == '__main__':
